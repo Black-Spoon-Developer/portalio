9c87393ebd80419619d30ebbb9af7ffc4aa92995
이진송 <annaring30@naver.com>
2024-10-21 17:52:00 +0900

    초기설정

---
+++ backend/Portalio/src/main/java/com/example/portalio/PortalioApplication.java
@@ -0,0 +1,13 @@
+package com.example.portalio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalioApplication.class, args);
+	}
+
+}
