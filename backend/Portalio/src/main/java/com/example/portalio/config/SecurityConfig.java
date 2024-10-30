59a829ed37ccf52b4aa3436f250983f402666480
김헌규 <khg6436@naver.com>
2024-10-30 23:53:35 +0900

    feat: 구글, 카카오 oAuth 설정 중

--- backend/Portalio/src/main/java/com/example/portalio/config/SecurityConfig.java
+++ backend/Portalio/src/main/java/com/example/portalio/config/SecurityConfig.java
@@ -24,15 +24,20 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .httpBasic(AbstractHttpConfigurer::disable) // HTTP 기본 인증을 비활성화
-                .cors() // CORS 활성화
-                .csrf(AbstractHttpConfigurer::disable)  // CSRF 보호를 비활성화합니다.
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 관리 정책을 STATELESS(세션이 있으면 쓰지도 않고, 없으면 만들지도 않는다)
+                .cors().disable() // CORS 활성화
+                .csrf(AbstractHttpConfigurer::disable)  // CSRF 보호를 비활성화
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.)) // 세션 관리 정책을 STATELESS(세션이 있으면 쓰지도 않고, 없으면 만들지도 않는다
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/v1/users/reissue").permitAll() // 토큰 발급 경로 허용
-                        .requestMatchers("/api/v1/users/")
+                        .requestMatchers("/api/v1/users/login", "/api/v1/users/signup").permitAll()
+                        .requestMatchers("/api/v1/recruiter/login", "/api/v1/recruiter/signup").permitAll()
                         .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/favicon.ico", "/h2-console/**").permitAll() // OAuth 로그인 사용
                         .anyRequest().authenticated() // 그 외 요청은 인증 필요
-                );
+                )
+                .oauth2Login() // OAuth2 로그인 시작
+                .userInfoEndpoint().userService(customOAuth2UserService) // OAuth 로그인시 사용자 정보를 가져오는 엔드포인트와 사용자 서비스를 설정
+                .failureHandler(oAuth2LoginFailureHandler) // OAuth2 로그인 실패시 처리할 핸들러 저장
+                .successHandler(oAuth2LoginSuccessHandler); // OAuth2 로그인 성공시 처리할 핸들러 저장
 
         // JWT 인증 필터를 UsernamePasswordAuthenticationFilter 앞에 추가한다.
         return http.addFilter(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
