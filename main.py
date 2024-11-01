39c3b1fa78c40ef2eb45e8fe7d5bb7c9019649c5
stylishy62 <stylishy62@gmail.com>
2024-11-01 10:07:18 +0900

    [AI/DT] main.py router 연결 세팅 중

---
+++ main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+from ai.question.service import router as question_router
+from ai.question.services import router as questions_router
+from ai.question.feedback import router as feedback_router
+from ai.spelling.grammar import router as grammar_router
+
+app = FastAPI()
+
