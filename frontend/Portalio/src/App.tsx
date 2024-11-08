ebf1d2aac24b4cefc8ee7ca2823f2be1b0cef49e
차상곤 <yg9618@naver.com>
2024-11-08 15:51:50 +0900

    ✨ 라우터 구조 설정 오류 해결
    라우터 구조 설정 오류 해결

--- frontend/Portalio/src/App.tsx
+++ frontend/Portalio/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "./components/common/UpNavBar.tsx";
+// import NavBar from "./components/common/UpNavBar.tsx";
 import Footer from "./components/common/Footer.tsx";
 import { Outlet, Route, Routes } from "react-router-dom";
 // import MainTestPage from "./pages/MainTest.tsx";
