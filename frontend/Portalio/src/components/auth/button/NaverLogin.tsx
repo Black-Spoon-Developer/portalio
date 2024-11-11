ee33aa1507c13068b0da072d842299ffac3ca898
김헌규 <khg6436@naver.com>
2024-11-12 00:04:55 +0900

    fixed: 배포 중 에러로 인한 부분 제거후 재커밋

--- frontend/Portalio/src/components/auth/button/NaverLogin.tsx
+++ frontend/Portalio/src/components/auth/button/NaverLogin.tsx
@@ -11,7 +11,7 @@ const NaverLogin: React.FC = () => {
 
     localStorage.setItem("isLogin", "true");
     // EC2에 띄워놓고 할 때
-    // window.location.href = "https://k11d202.p.ssafy.io/login/oauth2/code/naver";
+    window.location.href = "https://k11d202.p.ssafy.io/login/oauth2/code/naver";
 
     // 로컬에서 할때
     window.location.href = "http://localhost:8080/oauth2/authorization/naver";
