18c65832b7acd7abb034b8bf0781cf443078a3fb
이진송 <annaring30@naver.com>
2024-11-19 10:06:31 +0900

    dfdf

--- frontend/Portalio/src/api/AiInterviewCommonAPI.tsx
+++ frontend/Portalio/src/api/AiInterviewCommonAPI.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { BASE_URL } from "./BaseVariable";
+import { AI_BASE_URL } from "./BaseVariable";
 import { RootState } from "../store";
 import store from "../store";
 
@@ -8,7 +8,7 @@ export const getAiInterviewAnalysis = async (interview_id: number) => {
   const state: RootState = store.getState();
   const accessToken = state.auth.accessToken;
   const response = await axios.post(
-    `${BASE_URL}/api/v1/mock-interview/report/${interview_id}`,
+    `${AI_BASE_URL}/api/v1/mock-interview/report/${interview_id}`,
     {},
     {
       headers: {
